e1c265a5c02aec2e79b43bb9cef974ce3c177bfb
Carl Gruhn <cgruhn@bgsu.edu>
2020-04-12 10:47:42 -0400

    Project to do's

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+ 
+
+void main()
+{
+    //TODO: Step 1 - Ask user if they would like to start a new game
+    //TODO: Step 2 - Loop until user exits game
+    //TODO: Step 3 - Start game --> use Game class
+    //TODO: Step 4 - Initial roll, 5 dice (Randomize 5 values) --> Use Die/Dice class
+    //TODO: Step 5 - Display potential score based on dice rolls for each open category
+    //TODO: Setp 6 - User may select a category to score in, begin new round with category locked (Return to Step 4 w/ 1 fewer categories)
+    //TODO: Step 7 - User may select (or unselect) 0 to 5 dice to lock in for next round
+    //TODO: Step 8 - Repeat step 4 (max 2 times) with unlocked dice 
+    //TODO: Step 9 - Display final score
+    //TODO: Step 10 - Ask user to play again
+    //TODO              if yes, clear score and selected categories, repeat step 3 (or 4)
+    //TODO:             if no, exit game
+}
