3ab4897f7e09e58cc48e0937482f45f4a546e733
Carl Gruhn <cgruhn@bgsu.edu>
2020-04-18 17:00:43 -0400

    Setup Class ToDo's

---
+++ Game.hpp
@@ -0,0 +1,19 @@
+#pragma once Game.hpp
+
+class Game
+{
+private:
+    // TODO: hand variable -- from Dice class
+    // TODO: scoreboard variable -- from Scoreboard class
+public:
+    // TODO: play function -- function runs general game
+    // TODO: exit function
+
+    // TODO: Roll dice
+    // TODO:    Select dice (to keep / to reroll)
+    // TODO: Choose category
+    // TODO: Show score:
+    // TODO:    Potential category score to choose from (from open categories)
+    // TODO:    Current Score (from categories already scored)
+    // TODO:    Final Score
+};
