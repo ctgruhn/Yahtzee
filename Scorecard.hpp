0058b7afcbdc873735dd1e3a337d06494d6a8264
Carl Gruhn <cgruhn@bgsu.edu>
2020-04-18 17:16:27 -0400

    Added ToDo's

---
+++ Scorecard.hpp
@@ -0,0 +1,12 @@
+#pragma once Scorecard.hpp
+
+class Scorecard
+{
+private:
+
+public:
+    // TODO: Display scoreboard
+    // TODO:    Scoreboard based on roll
+    // TODO:    Current scoreboard
+    // TODO:    Final (or current total) Score
+};
