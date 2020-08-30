50ac672a6e3122b92de67705e05a0f127299ba2f
Carl Gruhn <cgruhn@bgsu.edu>
2020-08-30 12:18:22 -0400

    Print and set hand values for testing purposes

--- Dice.cpp
+++ Dice.cpp
@@ -7,3 +7,21 @@ void Dice::roll()
         hand[i].roll();
     }
 }
+
+void Dice::printHand()
+{
+    for (int i = 0; i < HAND_SIZE; i++)
+    {
+        std::cout << "Die-" << i + 1 << " = " << hand[i].getVal() << "\t";
+    }
+    std::cout<<std::endl;
+}
+
+void Dice::setHand(int die1, int die2, int die3, int die4, int die5)
+{
+    hand[0].setVal(die1);
+    hand[1].setVal(die2);
+    hand[2].setVal(die3);
+    hand[3].setVal(die4);
+    hand[4].setVal(die5);
+}
--- Dice.hpp
+++ Dice.hpp
@@ -1,5 +1,4 @@
-#ifndef Dice_hpp
-#define Dice_hpp
+#pragma once
 
 #include <iostream>
 #include <stdlib.h>
@@ -11,6 +10,7 @@ private:
     int val;
 public:
     const int getVal() {return val;}
+    void setVal (int v){val = v;}
     void roll(){ val = rand() % 6 + 1;};
 };
 
@@ -21,10 +21,10 @@ private:
     static const int HAND_SIZE = 5;
     Die hand[HAND_SIZE];
 public:
-    Dice(){ srand(time(0)); }  // Set seed for roll() rand function
+    Dice(){ srand(time(0)); }
     void roll();
     int getSize() { return HAND_SIZE;}
-    void printHand() { for (int i = 0; i < HAND_SIZE; i++){ std::cout << hand[i].getVal() << std::endl;}}
+    int getDice(int die) {return hand[die].getVal();}
+    void printHand();
+    void setHand(int die1, int die2, int die3, int die4, int die5);
 };
-
-#endif /* Dice_hpp */
