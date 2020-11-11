598b27bff44cc5c3170984af1df78a39edf1a3d4
Carl Gruhn <cgruhn@bgsu.edu>
2020-11-11 18:54:33 -0500

    Sort dice in hand

--- Dice.cpp
+++ Dice.cpp
@@ -1,5 +1,11 @@
 #include "Dice.hpp"
 
+bool operator<(Die const &die1, Die const &die2)
+{
+    return die1.val < die2.val;
+}
+
+
 void Dice::roll()
 {
     for (int i = 0; i < HAND_SIZE; i++)
@@ -24,4 +30,9 @@ void Dice::setHand(int die1, int die2, int die3, int die4, int die5)
     hand[2].setVal(die3);
     hand[3].setVal(die4);
     hand[4].setVal(die5);
+}
+
+void Dice::sort()
+{
+    std::sort(hand, (hand + HAND_SIZE));
 }
--- Dice.hpp
+++ Dice.hpp
@@ -12,6 +12,8 @@ public:
     const int getVal() {return val;}
     void setVal (int v){val = v;}
     void roll(){ val = rand() % 6 + 1;};
+    
+    friend bool operator<(Die const &die1, Die const &die2);
 };
 
 
@@ -25,6 +27,9 @@ public:
     void roll();
     int getSize() { return HAND_SIZE;}
     int getDice(int die) {return hand[die].getVal();}
+    
+    void sort();
+    
     void printHand();
     void setHand(int die1, int die2, int die3, int die4, int die5);
 };
